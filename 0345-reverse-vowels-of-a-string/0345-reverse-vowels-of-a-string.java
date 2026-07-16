dcbbb441a17b313c544bf76897f79600cb200fc0
Aswin <ceaswin146@gmail.com>
2026-07-16 22:52:37 +0530

    Time: 3 ms (71.76%), Space: 46.8 MB (34.28%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -10,9 +10,11 @@ class Solution {
             while(i<j && !isVowel(str[j])){
                 j--;
             }
+            if(i<j){
             char temp=str[i];
             str[i]=str[j];
             str[j]=temp;
+            }
             i++;
             j--;
         }
