d8c27df83417d204b821cf6f63fbfad93fcaeb18
Aswin <ceaswin146@gmail.com>
2026-07-31 20:23:41 +0530

    Time: 2 ms (24.28%), Space: 43.6 MB (5%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+
+        if (word.equals(word.toUpperCase())) {
+            return true;
+        }
+
+        if (word.equals(word.toLowerCase())) {
+            return true;   
+        }
+
+        String first = word.substring(0, 1).toUpperCase();
+        String rest = word.substring(1).toLowerCase();
+
+        return word.equals(first + rest);
+    }
+}
